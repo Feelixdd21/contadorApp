331f44b30828fa3fb01ad49399e99af44f6c2652
Alejandro Sanjuan <merumix@hotmail.com>
2023-10-29 13:12:02 -0600

    Se modifcaron los botones de registrar y recuperar contraseña, ademas de colocar correctamente los botones de regreso

--- src/app/auth/components/login/registro-usuarios/registro-usuarios.component.ts
+++ src/app/auth/components/login/registro-usuarios/registro-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-usuarios',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroUsuariosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  inicioSesion() {
+    this.router.navigate(['/']);
+  }
 }
