acdff5ef55cc6ddf63a44b0d749e18e7df0ae13f
felix valdes <fvaldes@bizprobpm.ca>
2023-11-05 00:31:05 -0600

    se agrego validacion para anfitrion y usuario

--- src/app/auth/components/login/inicio-sesion/inicio-sesion.component.ts
+++ src/app/auth/components/login/inicio-sesion/inicio-sesion.component.ts
@@ -20,7 +20,8 @@ export class InicioSesionComponent implements OnInit {
 
   onSubmit() {
     if (this.formulario.valid) {
-      if (this.formulario.get('usuario')?.value == 'admin' && this.formulario.get('pass')?.value == 'admin') {
+      if (this.formulario.get('usuario')?.value == 'anfitrion' && this.formulario.get('pass')?.value == 'anfitrion' || (this.formulario.get('usuario')?.value == 'user' && this.formulario.get('pass')?.value == 'user' )) {
+        localStorage.setItem('user', this.formulario.get('usuario')?.value)
         this.router.navigate(['/dashboard']);
       }
     }
