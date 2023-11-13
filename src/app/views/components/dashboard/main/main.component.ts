4d57a8feb7f7f0da7b637a833e11ee1f4741eb5d
felix valdes <fvaldes@bizprobpm.ca>
2023-11-12 21:38:40 -0600

    Se modifico el mensaje de bienvenido y el nombre de app

--- src/app/views/components/dashboard/main/main.component.ts
+++ src/app/views/components/dashboard/main/main.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+declare var anime: any;
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
   mensaje = "¡Bienvenido!";
   mostrarImagen: boolean = false;
 
@@ -16,7 +17,23 @@ export class MainComponent implements OnInit {
         this.mostrarImagen = this.deberiaMostrarImagen(event.url);
       }
     });
-   }
+  }
+  ngAfterViewInit(): void {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.ml13');
+    textWrapper!.innerHTML = textWrapper!.textContent!.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({ loop: false })
+      .add({
+        targets: '.ml13 .letter',
+        translateY: [100, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el: any, i: number) => 300 + 30 * i
+      })
+  }
 
   ngOnInit(): void {
   }
